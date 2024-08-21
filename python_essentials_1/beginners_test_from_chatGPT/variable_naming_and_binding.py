7008a33300fb42116d5e35ab64eaaa194ff8df13
Lawson Israel Pascal <isreallawson@outlook.com>
2024-08-21 15:05:47 +0000

    test_3

---
+++ python_essentials_1/beginners_test_from_chatGPT/variable_naming_and_binding.py
@@ -0,0 +1,25 @@
+'''
+Test 3:
+Create variables that store the following information about a book and ensure
+the variable names follow Python naming conventions:
+
+Title: "The Catcher in the Rye"
+Author: "J.D. Salinger"
+Year of Publication: 1951
+Price: 9.99
+After assigning the variables, print the information using the following
+format:
+
+plaintext
+Copy code
+The book 'The Catcher in the Rye' was written by J.D. Salinger in 1951.
+It costs $9.99.
+'''
+
+title = "The Catcher in the Rye"
+author = "J.D. Salinger"
+year_of_publication = 1951
+price = 9.99
+
+print(f"The book '{title}' was written by {author} in {year_of_publication}.\
+ It costs ${price}.")

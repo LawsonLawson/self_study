7adcab35b7a13b0e659b82e05e60dc3f21e72af0
Lawson Israel Pascal <isreallawson@outlook.com>
2024-08-21 19:27:12 +0000

    test_6

---
+++ python_essentials_1/beginners_test_from_chatGPT/combining_multiple_concepts.py
@@ -0,0 +1,32 @@
+'''
+Test 6:
+Write a program that accepts two numbers from the user
+(they can be integers or floats). It should:
+
+Calculate their sum, difference, and product.
+Display the results in a formatted manner where each value is right-aligned in
+a width of 10 spaces.
+Example input:
+
+plaintext
+Copy code
+Enter first number: 5
+Enter second number: 2
+Expected output:
+
+plaintext
+Copy code
+Sum:              7
+Difference:       3
+Product:         10
+'''
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))
+
+_sum = first_number + second_number
+_difference = first_number - second_number
+_product = first_number * second_number
+
+print("Sum: {:>10}".format(_sum))
+print("Difference: {:>10}".format(_difference))
+print("Product: {:>10}".format(_product))

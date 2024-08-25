f8342102862554290da3a2afb19da531c847d5d6
Lawson Israel Pascal <isreallawson@outlook.com>
2024-08-25 01:32:33 +0000

    solution for lab_10

---
+++ python_essentials_1/lab_10.py
@@ -0,0 +1,76 @@
+'''
+Scenario
+Listen to this story: a boy and his father, a computer programmer, are playing
+with wooden blocks. They are building a pyramid.
+
+Their pyramid is a bit weird, as it is actually a pyramid-shaped wall -
+it's flat. The pyramid is stacked according to one simple principle: each lower
+layer contains one block more than the layer above.
+
+The figure illustrates the rule used by the builders:
+
+
+
+Your task is to write a program which reads the number of blocks the builders
+have, and outputs the height of the pyramid that can be built using these
+blocks.
+
+Note: the height is measured by the number of fully completed layers - if the
+builders don't have a sufficient number of blocks and cannot complete the next
+layer, they finish their work immediately.
+
+Test your code using the data we've provided.
+
+
+Test Data
+
+Sample input: 6
+
+Expected output: The height of the pyramid: 3
+
+Sample input: 20
+
+Expected output: The height of the pyramid: 5
+
+Sample input: 1000
+
+Expected output: The height of the pyramid: 44
+
+Sample input: 2
+
+Expected output: The height of the pyramid: 1
+'''
+# prompt user for input
+try:
+    user_input = int(input("Enter the number of blocks: "))
+
+    # check if the input is zero or less
+    if user_input <= 0:
+        print("You cannot build a pyramid with {} blocks".format(user_input))
+
+        # and exit the program
+        print("Enter a positive interger please")
+        exit()
+    elif user_input == 1 or user_input == 2:
+        height = 1
+        print("The height of the pyramind: {}\nTechnically,\
+ this isn't a pyramid by the way ..".format(height))
+        exit()
+
+    else:
+        # if user input is a positive integer .. we start building pyramid ..
+        height = 0
+
+        # build pyramind starting with one block ..
+        for count in range(1, user_input):
+
+            # so we subract 1 on first iteration and increase height ..
+            # and do same but the previous + 1 (2, 3, 4 ...)
+
+            user_input = user_input - count
+            if user_input >= 0:  # check if input is greater or equal to 0
+
+                height += 1  # then increase height by 1
+        print("The height of the pyramid: {}".format(height))
+except ValueError:
+    print("Please enter a valid integers .. I cannot work with strings")
